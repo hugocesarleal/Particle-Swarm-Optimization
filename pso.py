60a95c9b11a8f42f7fd60eeb372e335c69ac81df
Hugo <hugocesarlleal@gmail.com>
2024-12-18 10:04:56 -0300

    v4

--- pso.py
+++ pso.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 from visualizacao import atualizar_grafico, inicializar_grafico
 
 def fit(x):
@@ -29,6 +30,7 @@ def calcularVelocidades(w, c1, c2, particulas, velocidades, pBest, gBest, nDimen
     novasVelocidades = []
     cognitivo = [tuple(x1 - x2 for x1, x2 in zip(tupla1, tupla2)) for tupla1, tupla2 in zip(pBest, particulas)]
     social = [tuple(x - y for x, y in zip(gBest, tupla)) for tupla in particulas]
+    
 
     i = 0
     for v in velocidades:
@@ -68,7 +70,7 @@ def pso(maxIter, w, c1, c2, qtdeParticulas, nDimensoes, limInf, limSup, plotar):
                 gBest = novaPosicao
 
         if plotar:
-            atualizar_grafico(ax, particulas, iter, intervalo=1.1)
+            atualizar_grafico(ax, particulas, iter, intervalo=0.1)
 
         ftMedia.append(sum(fitness) / len(fitness))
         ftgBest.append(fit(gBest))
@@ -82,14 +84,14 @@ def arredondar_solucao(tupla, casas_decimais=4):
     return tuple(round(coordenada, casas_decimais) for coordenada in tupla)
 
 if __name__ == "__main__":
-    maxIter = 20
+    maxIter = 50
     w = 0.5
     c1 = 1.3
     c2 = 1.5
     qtdeParticulas = 5000
     nDimensoes = 2
-    limInf = -5
-    limSup = 5
+    limInf = -5.12
+    limSup = 5.12
     plotar = True
 
     sol = pso(maxIter, w, c1, c2, qtdeParticulas, nDimensoes, limInf, limSup, plotar)
